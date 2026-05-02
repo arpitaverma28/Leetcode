d7479c4050ac1f3be977019df74424f6d3a6f94c
arpitaverma28 <arpitaverma13578@gmail.com>
2026-05-02 11:10:04 +0530

    Time: 5 ms (97.90%) | Memory: 44.1 MB (63.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,24 +1,28 @@
 class Solution {
+    public boolean allZero(int arr[]){
+        for(int i =0;i<arr.length;i++){
+            if(arr[i] !=0) return false;
+        }
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
         if(n1>n2) return false;
-       HashMap<Character,Integer> hm1 = new HashMap<>();
-       HashMap<Character,Integer> hm2 = new HashMap<>();
+      int arr[] = new int[26];
         for(char c : s1.toCharArray()){
-           hm1.put(c,hm1.getOrDefault(c,0)+1);
+          arr[c-'a']++;
         }
         int i =0;
         for(int j =0;j<n2;j++){
-            char c = s2.charAt(j);
-            hm2.put(c,hm2.getOrDefault(c,0)+1);
-            if(j-i+1 >n1){
-                    char out = s2.charAt(i);
-                    hm2.put(out,hm2.get(out)-1);
-                    if(hm2.get(out)==0) hm2.remove(out);
-                    i++;
-            }
-            if(hm1.equals(hm2)) return true;
+            arr[s2.charAt(j)-'a']--;
+        
+        if(j-i+1>n1){
+             arr[s2.charAt(i)-'a']++;
+             i++;
+          }
+
+          if(allZero(arr)) return true;
         }
         return false;
     }
