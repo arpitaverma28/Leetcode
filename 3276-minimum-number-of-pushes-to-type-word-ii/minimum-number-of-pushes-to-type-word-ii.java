fd97b3769a1783de87d0c4939ec020c1c5174231
arpitaverma28 <arpitaverma13578@gmail.com>
2026-07-31 10:51:57 +0530

    Time: 10 ms (66.76%) | Memory: 47.9 MB (73.70%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,33 +1,16 @@
 class Solution {
-    public class Pair implements Comparable<Pair>{
-        char c ;
-        int f;
-        Pair(char c , int f){
-            this.c = c;
-            this.f =f;
-        }
-        public int compareTo(Pair p){
-            return p.f - this.f;
-        }
-    }
     public int minimumPushes(String word) {
-        int ans =0;
-        HashMap<Character,Integer> hm = new HashMap<>();
+        int fre[] = new int[26];
         for(char c : word.toCharArray()){
-            hm.put(c,hm.getOrDefault(c,0)+1);
+            fre[c-'a']++;
         }
-         PriorityQueue<Pair> pq = new PriorityQueue<>();
-         for(char c : hm.keySet()){
-            pq.add(new Pair(c,hm.get(c)));
-         }
-
-         int count =0;
-         int s = pq.size();
-         for(int i =0;i<s;i++){
-            Pair p = pq.poll();
-            ans += ((i/8)+1)*p.f;
-            }
+        Arrays.sort(fre);
+        int ans=0;
+        int idx=0;
+        for(int i =25;i>=0 && fre[i]>0;i--){
+            ans += fre[i] *(idx/8 +1);
+            idx++;
+        }
         return ans;
-         }
-        
     }
+}
